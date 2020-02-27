071e319dd88147db08555ac63c30180c0f787247
neer2808 <uic.neerajkhanna@gmail.com>
2020-02-27 11:49:54 +0530

    Add two methods to add a new node in the tree and to traverse the tree (level order traversal)

--- src/BinaryTreewithLinkedList.java
+++ src/BinaryTreewithLinkedList.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreewithLinkedList
 {
   Node root;
@@ -14,12 +17,50 @@ public class BinaryTreewithLinkedList
       root = newnode;
       return;
     }
-
-
+    // Taking help of Collection frame work to implement it with queue
+    // you can also use your own custorm class that contains implementation Methods of queue
+    Queue<Node> q= new LinkedList<Node>();
+    q.add(newnode);
+    while(!q.isEmpty())
+    {
+        Node presentnode = q.remove();  // performing dequeue operation to remove node from queue
+        if(presentnode.left== null)
+        {
+          presentnode.left = newnode;
+          break;
+        }
+        else if(presentnode.right== null)
+        {
+          presentnode.right= newnode;
+        }
+        else
+        {
+          q.add(presentnode.left);
+          q.add(presentnode.right);
+        }
+    }
   }
 
   public void levelTraversal()
   {
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+    while(!q.isEmpty())
+    {
+      Node presentnode = q.remove();
+      System.out.println(presentnode.data);
+
+      if(presentnode.left != null)
+      {
+          q.add(presentnode.left);
+      }
+      if(presentnode.right!=null)
+      {
+        q.add(presentnode.right);
+      }
+
+    }
+
 
 
   }
